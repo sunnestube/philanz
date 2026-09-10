0e14ae8ec4e6e4f72ef21feabc8a48da80d8b912
Ajne <kita@jueni.li>
2026-09-10 18:42:11 +0200

    fix(year): Patch-Import und Konstanten-Template

--- bila/src/app/component/year/year.component.ts
+++ bila/src/app/component/year/year.component.ts
@@ -10,6 +10,7 @@ import {CELL_TYPE} from '../../model/CellType';
 import {ImportComponent} from '../import/import.component';
 import {WorkbookService, YearView} from '../../service/workbook.service';
 import {YearArchiveService, YearMeta} from '../../service/year-archive.service';
+import '../../service/workbook-years.patch';
 import {SaldoPanelComponent} from '../saldoPanel/saldo-panel.component';
 import {StartTabComponent} from '../startTab/start-tab.component';
 import {ConstantsTabComponent} from '../constantsTab/constants-tab.component';
